233dcd65d4333b0e3ed3c2ad44500d72e0303c76
Chris Siannas <xrsia@otenet.gr>
2022-02-01 13:47:23 +0200

    sendTicket(...)

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 		if isValidName && isValidEmail && isValidTickerNumber {
 
 			bookings=bookTicket(userTickets, firstName, lastName, email)
-
+			sendTicket(userTickets, firstName, lastName, email)
 			firstNames := getFirstNames()
 
 			fmt.Printf("The first names of booking are: %v\n", firstNames)
@@ -111,4 +111,10 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remained for %v\n", remainingTickets, conferenceName)
 	return bookings
+}
+func sendTicket(userTickets uint ,firstName,lastName,email string){
+	var ticket = fmt.Sprintf("%v tickets for %v %v",userTickets,firstName,lastName)
+	fmt.Println("##########################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n",ticket,email)
+	fmt.Println("##########################")
 }
